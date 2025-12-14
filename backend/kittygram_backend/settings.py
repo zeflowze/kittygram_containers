f3cf8e22a34a61403bd6923a15ae7e2ce9ec0294
zeflowze <117173325+zeflowze@users.noreply.github.com>
2025-12-14 19:22:08 +0500

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,22 +3,33 @@ import os
 import sys
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 from kittygram_backend.utils import db, on_or_off
 
 
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Не проходят тесты если ключ в env файле
 
+# SECRET_KEY
 KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', KEY) or sys.exit(
+    'Need SECRET KEY for correct work'
+)
 
-SECRET_KEY = os.getenv('SECRET_KEY', KEY) or sys.exit("Need SECRET KEY for correct work")
 
+# DEBUG
 DEBUG = on_or_off(os.getenv('TRIGGER'))
 
-ALLOWED_HOSTS = [
-    os.getenv('ALLOWED_HOST', '*'),
-]
+
+# ALLOWED_HOSTS
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1'
+).split(',')
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -27,11 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework.authtoken',
+
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
+
     'cats.apps.CatsConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -43,7 +57,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'kittygram_backend.urls'
+
 
 TEMPLATES = [
     {
@@ -61,26 +77,35 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
+
+# DATABASES
 DATABASES = {
     'default': db(os.getenv('NAME_DATABASE'))
 }
 
+
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -93,24 +118,26 @@ USE_L10N = True
 USE_TZ = True
 
 
+# STATIC & MEDIA
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static_backend/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/media/'
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# DRF
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
